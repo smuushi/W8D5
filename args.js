89f023598e45cf47f148e3b06b6dc95cedddd25d
smuushi <mishih780@gmail.com>
2023-03-03 15:29:38 -0500

    asdf

--- args.js
+++ args.js
@@ -10,14 +10,14 @@ const sum = function () {
 
 const sum2 = function (...args) {
   let total = 0;
-  args.forEach( (el) => total += el)
+  args.forEach( (el) => total += el);
   return total;
 }
 // console.log(sum2(1, 2, 3, 4))
 
 Function.prototype.myBind = function(ctx, ...bindArgs){
   
-  let that = this
+  let that = this;
   // let arguments = all the extra stuff... 
 
   if (bindArgs.length === 0) {
@@ -26,7 +26,7 @@ Function.prototype.myBind = function(ctx, ...bindArgs){
     };
   } else {
     return function() {
-      that.call(ctx, ...bindArgs, ...arguments)
+      that.call(ctx, ...bindArgs, ...arguments);
     };
   };
 
@@ -36,9 +36,9 @@ let func = function (arg1, arg2) {
   // console.log( `${this.name}`)
   debugger
   if (arguments.length > 0) {
-    console.log(`${this.name} does ${arg1} and ${arg2}`)
+    console.log(`${this.name} does ${arg1} and ${arg2}`);
   } else {
-    console.log(`${this.name}`)
+    console.log(`${this.name}`);
   }
 }
 
@@ -48,5 +48,30 @@ let theirBind = func.bind({name: 'their_bind'}, "asddddf")
 
 // bindTest("asdf")
 
-theirBind()
+// theirBind()
 
+
+
+function curriedSum(numArgs) {
+
+  let numbers = [];
+
+  return function(newNumber) {
+    if (numbers.length < numArgs) {
+      numbers.push(newNumber);
+    } else {
+      return sum2(numbers)
+    };
+  }
+  // while (numbers.length < numArgs){
+  //   return function(newNumber) {
+  //     numbers.push(newNumber);
+  //   };
+  // }
+
+  // return numbers.sum2()
+}
+
+const summ = curriedSum(4)
+
+// console.log(summ(5)(30)(20)(1))
